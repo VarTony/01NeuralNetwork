1efab87561e1e2b2726d0af9a40840e67154e340
Anion <littlesalo3@gmail.com>
2018-12-18 01:58:15 +0300

    0.2

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,4 +1,9 @@
-class NeuralNetwork:
+
+
+import numpy 
+import scipy.special
+
+class neuralNetwork:
   
   def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
     self.inodes = inputnodes
@@ -6,12 +11,33 @@ class NeuralNetwork:
     self.onodes = outputnodes
     # коффициент обучения
     self.lr = learningrate
+    # Матрицы весовых коэффициентов связей wih (между входным и скрытым слоями)и who (между скрытыми и выходными слоями). 
+    # Весовые коэффициенты связей между узлом i и j следующего слоя обозначены как w_i_j:
+    # w11 w21
+    # w12 w22 и т.д.
+    self.wih = numpy.random.normal(0.0, pow(self.hnodes, - 0.5), (self.hnodes, self.inodes))
+    self.who = numpy.random.normal(0.0, pow(self.onodes, - 0.5), (self.onodes, self.hnodes))
+    
+    #Функсия активации (сигмойда)
+    self.activation_function = lambda x: scipy.special.expit(x)
     pass
 
   def train():
     pass
   
-  def query():
+  def query(self, inputs_list):
+    
+    #Преобразование входных данных в двухмерный массив
+    inputs = numpy.array(inputs_list, ndmin=2).T
+    
+    hidden_inputs = numpy.dot(self.wih, inputs)
+    #Применение к сигналам сглаженым весовыми коэффициентами функции активации.
+    hidden_outputs = self.activation_function(hidden_inputs)
+    
+    final_inputs = numpy.dot(self.who, hidden_outputs)
+
+    final_outputs = self.activation_function(final_inputs)
+    return final_outputs
     pass
 
 # количество входных, скрытых и выходных данных
@@ -21,4 +47,9 @@ output_nodes = 3
 # коэффициент обучения равен 0.3
 learning_rate = 0.3
 # создать экземпляр нейронной сети
-n = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+
+print(n.query([0.8543, 12.355, 4.20123]))
+
+print(numpy.random.normal(0.0, pow(hidden_nodes, - 0.5), 
+(hidden_nodes, input_nodes))) #Ok
